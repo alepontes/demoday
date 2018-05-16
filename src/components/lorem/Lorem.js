35614d09a4eed225b9f29d0eaf1fc4440b2f69b9
unknown <alexandrelopespontes@gmail.com>
2018-05-16 11:17:53 -0300

    Melhorar menu

--- src/components/lorem/Lorem.js
+++ src/components/lorem/Lorem.js
@@ -28,6 +28,49 @@ class Lorem extends Component {
           laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
           voluptate velit esse cillum dolore e"
         />
+
+        <ItemLorem 
+          type={true} 
+          imagem="http://www.stackoverflowbusiness.com/hubfs/B2B-SO/Sales/Images/Home/Educate-BuildAwareness.svg?t=1525446690975" 
+          icon="https://www.stackoverflowbusiness.com/hubfs/B2B-SO/Sales/Images/Home/Educate.svg?t=1525446690975" 
+          topico="Topico" 
+          titulo="Titulo onsectetur adipiscing elit" 
+          texto=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore e"
+        />
+
+        <ItemLorem 
+          type={false} 
+          imagem="http://www.stackoverflowbusiness.com/hubfs/B2B-SO/Sales/Images/Home/Educate-BuildAwareness.svg?t=1525446690975" 
+          icon="https://www.stackoverflowbusiness.com/hubfs/B2B-SO/Sales/Images/Home/Educate.svg?t=1525446690975" 
+          topico="Topico" 
+          titulo="Titulo onsectetur adipiscing elit" 
+          texto=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore e"
+        />
+        
       </div>
     );
 
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Nav extends Component {
+
+  componentWillMount(){
+    this.setState({bg: "trans"})
+  }
+
+  componentDidMount(){
+    window.onscroll = this.handleScroll;
+  }
+
+  componentWillUnmount(){
+    window.onscroll = null;
+  }
+
+
+  handleScroll = () => {
+    console.log(this);
+    if(window.scrollY <40 ){
+      this.setState({bg: "trans"})
+    }
+    else{
+      this.setState({bg: "solid"})
+    }
+  }
+
   render() {
     return (
       <div className="nav">
-        <div className="menu">
+        <div className="menu" id={this.state.bg}>
           <img src="http://themes.startbootstrap.com/vitality-v2.1.0/img/agency/logo.svg" alt="" />
 
           <ul>
